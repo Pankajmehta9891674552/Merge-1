d4bbc22f10cb03bc90feafc3441a0c84c3f0af8e
Pankajmehta9891674552 <pankaj.mehta989@gmail.com>
2024-01-06 23:06:17 +0530

    3hain

--- src/test/java/PACKAGE2/B.java
+++ src/test/java/PACKAGE2/B.java
@@ -1,5 +1,7 @@
 package PACKAGE2;
 
+import java.util.Scanner;
+
 import org.testng.annotations.Test;
 
 public class B {
@@ -9,4 +11,29 @@ public class B {
 	{
 		System.out.println("B calling");
 	}
+	
+	
+	@Test
+	public void B1()
+	{
+		System.out.println("B1 calling");
+		
+		Scanner sc =new Scanner(System.in);
+		   
+		   System.out.println("enter sie of array");
+		   int size=sc.nextInt();	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
