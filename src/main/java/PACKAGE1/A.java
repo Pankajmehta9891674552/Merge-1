42643e1b9a1be17e6e1866dea438b3b1d2d75332
Pankajmehta9891674552 <pankaj.mehta989@gmail.com>
2024-01-06 22:57:36 +0530

    first

---
+++ src/main/java/PACKAGE1/A.java
@@ -0,0 +1,25 @@
+package PACKAGE1;
+
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+public class A {
+
+	
+	@Test
+	public void A()
+	{
+		System.out.println("A calling");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/PACKAGE2/B.java
@@ -0,0 +1,12 @@
+package PACKAGE2;
+
+import org.testng.annotations.Test;
+
+public class B {
+
+	@Test
+	public void B()
+	{
+		System.out.println("B calling");
+	}
+}
