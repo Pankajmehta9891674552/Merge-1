39a4cfb1a78a1f84225224aa2e11efdfe0d2ffc8
Pankajmehta9891674552 <pankaj.mehta989@gmail.com>
2024-01-06 23:01:46 +0530

    feature 2

--- src/main/java/PACKAGE1/A.java
+++ src/main/java/PACKAGE1/A.java
@@ -15,7 +15,16 @@ public class A {
 	
 	
 	
-	
+	@Test
+	public void A1()
+	{
+		System.out.println("A1 calling");
+		
+		Scanner sc =new Scanner(System.in);
+		   
+		   System.out.println("enter sie of array");
+		   int size=sc.nextInt();	
+	}
 	
 	
 	
